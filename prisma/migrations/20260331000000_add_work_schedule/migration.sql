757772da89d8738b098358b74b32fdd9731093c0
webster-rafael <webster.rafael2014@gmail.com>
2026-03-31 17:24:49 -0400

    feat: Add latitude and longitude fields to patrol check-ins and store them during creation.

---
+++ prisma/migrations/20260331000000_add_work_schedule/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "public"."users" ADD COLUMN "work_scale" TEXT,
+ADD COLUMN "entry_time" TEXT,
+ADD COLUMN "lunch_start" TEXT,
+ADD COLUMN "lunch_end" TEXT,
+ADD COLUMN "exit_time" TEXT;
